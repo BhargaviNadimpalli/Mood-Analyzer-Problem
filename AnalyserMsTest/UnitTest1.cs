e9dc5f21f29213cd8ec694a1063477d147825f76
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-23 10:27:15 -0600

    feat:Given happy mood by constructor parameters

--- AnalyserMsTest/UnitTest1.cs
+++ AnalyserMsTest/UnitTest1.cs
@@ -7,10 +7,10 @@ namespace AnalyserMsTest
     {
 
         [TestMethod]
-        public void GiveSadMoodReturnSad()
+        public void GiveHappyMoodReturnHappy()
         {
-            string expected = "Sad";
-            string message = "I am in sad mood";
+            string expected = "Happy";
+            string message = "I am in happy mood";
             MoodAnalyse analyse = new MoodAnalyse(message);
 
             string mood = analyse.AnalyseMood();
