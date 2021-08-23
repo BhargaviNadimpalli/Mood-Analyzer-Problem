e5786de919799f8942a389da824dfae1b95d5de7
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-23 10:13:34 -0600

    feat: Given sad message and return Sad

--- AnalyserMsTest/UnitTest1.cs
+++ AnalyserMsTest/UnitTest1.cs
@@ -7,10 +7,10 @@ namespace AnalyserMsTest
     {
 
         [TestMethod]
-        public void GivehappyMoodReturnhappy()
+        public void GiveSadMoodReturnSad()
         {
-            string expected = "Happy";
-            string message = "I am in happy mood";
+            string expected = "Sad";
+            string message = "I am in sad mood";
             MoodAnalyse analyse = new MoodAnalyse();
 
             string mood = analyse.AnalyseMood(message);
